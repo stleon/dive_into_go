4a3652f628bc7d94c57c95d1cfd1262408c42a75
Lev Tonkikh <leonst998@gmail.com>
2019-06-10 21:15:30 +0300

    write to stdin

---
+++ parent.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+)
+
+
+func main() {
+
+     cmd := exec.Command("go", "run", "first.go")
+
+     stdin, err := cmd.StdinPipe()
+     if err != nil {
+	log.Fatal(err)
+     }
+
+
+     go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, "values written to stdin are passed to cmd's standard input")
+     }()
+
+     out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", out)
+}
